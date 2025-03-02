15c25d66c005573008a1e6a193b8d3133e92293e
Julian Pineda <918851+julianpinedayyz@users.noreply.github.com>
2025-03-02 09:37:44 -0500

    Add Dracula theme color palette to Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,32 +7,19 @@ export default {
   theme: {
     extend: {
       colors: {
-        // Dracula Theme (Dark)
-        dracula: {
-          background: '#282A36',
-          currentLine: '#44475A',
-          foreground: '#F8F8F2',
-          comment: '#6272A4',
-          cyan: '#8BE9FD',
-          green: '#50FA7B',
-          orange: '#FFB86C',
-          pink: '#FF79C6',
-          purple: '#BD93F9',
-          red: '#FF5555',
-          yellow: '#F1FA8C'
-        },
-        // Ghostty Theme (Light)
-        ghostty: {
-          background: '#FFFFFF',
-          foreground: '#11111B',
-          comment: '#7F849C',
-          cyan: '#209FB5',
-          green: '#40A02B',
-          orange: '#FE640B',
-          pink: '#EA76CB',
-          purple: '#8839EF',
-          red: '#D20F39',
-          yellow: '#DF8E1D'
+        'dracula': {
+          'background': '#282a36',
+          'currentLine': '#44475a',
+          'selection': '#44475a',
+          'foreground': '#f8f8f2',
+          'comment': '#6272a4',
+          'cyan': '#8be9fd',
+          'green': '#50fa7b',
+          'orange': '#ffb86c',
+          'pink': '#ff79c6',
+          'purple': '#bd93f9',
+          'red': '#ff5555',
+          'yellow': '#f1fa8c'
         }
       }
     },
