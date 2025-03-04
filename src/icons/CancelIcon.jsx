f1e8da1b76c168b052e92a3b2df3f68e9646abb3
Julian Pineda <918851+julianpinedayyz@users.noreply.github.com>
2025-03-04 13:09:51 -0500

    feat: Update all app icons to support Octicons
    - Implemented dual-library support for all icon components
    - Added Octicons alternatives for Delete, Edit, Save and Cancel actions
    - Updated ThemeToggle to use Octicons sun and moon icons
    - Updated StatusBar to use Octicons for status indicators
    - Connected all icons to IconContext for consistent toggling
    - Maintained visual consistency across both icon libraries

--- src/icons/CancelIcon.jsx
+++ src/icons/CancelIcon.jsx
@@ -1,7 +1,15 @@
-const CancelIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4" aria-hidden="true" focusable="false">
-    <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
-  </svg>
-)
+import { useIconLibrary } from '../contexts/IconContext';
+import { XIcon } from '@primer/octicons-react';
+import { FaTimes } from 'react-icons/fa';
 
-export default CancelIcon
+function CancelIcon({ size = 16 }) {
+  const { useOcticons } = useIconLibrary();
+
+  return useOcticons ? (
+    <XIcon size={size} />
+  ) : (
+    <FaTimes size={size} />
+  );
+}
+
+export default CancelIcon;
