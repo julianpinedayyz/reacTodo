97dcd0fa4e21b7349fe46529a49eef573b25d514
Julian Pineda <918851+julianpinedayyz@users.noreply.github.com>
2025-03-04 11:46:55 -0500

    feat: Integrate IconProvider in application
    - Added IconProvider to main application wrapper
    - Nested IconProvider inside ThemeProvider for proper context hierarchy
    - Enables icon library switching functionality across app

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { ThemeProvider } from './contexts/ThemeContext.jsx'
+import { ThemeProvider } from './contexts/ThemeContext'
+import { IconProvider } from './contexts/IconContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <IconProvider>
+        <App />
+      </IconProvider>
     </ThemeProvider>
   </React.StrictMode>,
 )
