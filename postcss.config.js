22643262681ef1239f537102fcdc643e37aa076d
Julian Pineda <918851+julianpinedayyz@users.noreply.github.com>
2025-03-04 23:32:29 -0500

    feat: enhance PostCSS configuration with nested and preset-env plugins
    Updated PostCSS configuration to support nested CSS syntax and modern CSS features

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
 export default {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    'tailwindcss': {},
+    'postcss-nested': {},
+    'postcss-preset-env': {
+      features: {
+        'nesting-rules': false, // Disable nesting-rules in preset-env as we're using postcss-nested
+      },
+    },
+    'autoprefixer': {},
   },
 }
